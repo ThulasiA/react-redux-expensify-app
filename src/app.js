e25a7da0b67fc443649066b3afba2cd342d726a6
Thulasi <atfornet@gmail.com>
2018-10-08 21:36:54 -0700

    Setup Production Webpack

--- src/app.js
+++ src/app.js
@@ -3,23 +3,12 @@ import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import getVisibleExpenses from './selectors/expenses';
 import 'react-dates/initialize';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
-store.dispatch(addExpense({description:'Water', amount: 1000}));
-store.dispatch(addExpense({description:'Gas Bill', amount: 3000, createdAt: 10000000}));
-store.dispatch(addExpense({description:'Rent', amount: 150000}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-console.log('finding error');
-console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,40 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract = new MiniCssExtractPlugin({
+    filename: 'styles.css'
+  });
+  return {
+    entry: './src/app.js',
+    output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    },{
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  },
-  mode: 'none'
-}
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },{
+        test: /\.s?css$/,
+        use: [   
+          MiniCssExtractPlugin.loader,       
+          { loader: 'css-loader', options: { sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true } }
+        ]
+      }]
+    },
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    plugins: [
+      CSSExtract
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    },
+    mode: 'none'
+    }  
+};
 
