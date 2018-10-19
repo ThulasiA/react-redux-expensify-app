de1df1cd9bfe09e8bda80821ef03835938149c36
Thulasi <atfornet@gmail.com>
2018-10-18 20:41:18 -0700

    Setup test database env

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,17 +1,24 @@
 import uniqid from 'uniqid';
+import database from '../firebase/firebase';
 
 //ADD_EXPENSE Action Generator
-export const addExpense = ({description = '', note = '', amount = 0, createdAt = 0} = {}) => ({
+export const addExpense = (expense) => ({
   type:'ADD_EXPENSE',
-  expense: {
-    id: uniqid(),
-    description,
-    note,
-    amount,
-    createdAt
-  }
+  expense
 });
-  
+
+export const startAddExpense = (expenseData = {}) => {
+  return (dispatch) => {
+    const { description = '', note = '', amount = 0, createdAt = 0 } = expenseData;
+    const expense = { description, note, amount, createdAt };
+    return database.ref('expenses').push(expense).then((ref) => {
+      dispatch(addExpense({
+        id: ref.key,
+        ...expense
+      }));
+    });
+  };
+};
 //REMOVE EXPENSE
 export const removeExpense = ({ id } = {}) => ({
   type:'REMOVE_EXPENSE',
